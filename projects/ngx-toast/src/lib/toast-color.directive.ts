ddd68c1e648e6fb0327eca0df2653d4bb0499967
ilDug <dognini.marco@gmail.com>
2024-02-27 17:16:04 +0100

    styling icons and colors

--- projects/ngx-toast/src/lib/toast-color.directive.ts
+++ projects/ngx-toast/src/lib/toast-color.directive.ts
@@ -9,8 +9,8 @@ export class ToastColorDirective {
 
   @Input('toastColor') toast: DToast;
 
-  @HostBinding('class.text-info') get info(): boolean { return this.toast ? this.toast.type === 'info' : false }
-  @HostBinding('class.text-warning') get warning(): boolean { return this.toast ? this.toast.type === 'warning' : false }
-  @HostBinding('class.text-danger') get error(): boolean { return this.toast ? this.toast.type === 'error' : false }
+  @HostBinding('class.t-main') get info(): boolean { return this.toast ? this.toast.type === 'info' : false }
+  @HostBinding('class.t-warn') get warning(): boolean { return this.toast ? this.toast.type === 'warning' : false }
+  @HostBinding('class.t-error') get error(): boolean { return this.toast ? this.toast.type === 'error' : false }
 
 }
