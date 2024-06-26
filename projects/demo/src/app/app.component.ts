67bafde37b73b21a46666f983321e7a7dbd89e1c
ilDug <dognini.marco@gmail.com>
2024-06-26 17:34:31 +0200

    refactoring the logic of DEMO

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { NgxToastModule } from 'ngx-toast';
 import { NgxToastService } from 'ngx-toast';
+import { concatMap, delay, from, of } from 'rxjs';
 
 @Component({
     selector: 'app-root',
@@ -11,12 +12,29 @@ import { NgxToastService } from 'ngx-toast';
     imports: [CommonModule, NgxToastModule]
 })
 export class AppComponent {
+
     private toast = inject(NgxToastService)
 
+    // Your array
+    private items = [
+        { message: "May the Force be with you.", type: 'info' },
+        { message: "These aren't the droids you're looking for.", type: 'error' },
+        { message: "Do or do not. There is no try.", type: 'warning' },
+    ];
+
+    // Interval time in milliseconds
+    private intervalTime = 1000;
+    private closeAfter = 5500;
+
+    // Creating an Observable that emits each item of the array after a certain interval
 
     open() {
-        this.toast.info("May the Force be with you.", 550000);
-        this.toast.error("These aren't the droids you're looking for. ", 550000);
-        this.toast.warning("Do or do not. There is no try.", 550000);
+        from(this.items)
+            .pipe(
+                concatMap(item => of(item).pipe(delay(this.intervalTime)))
+            )
+            .subscribe(item =>
+                this.toast[item.type](item.message, this.closeAfter)
+            )
     }
 }
